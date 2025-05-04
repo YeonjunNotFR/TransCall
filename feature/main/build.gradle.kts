686f3ff22f9cd05ab08434506777d99eecefdc41
yeonjun <kgbduswns@gmail.com>
2025-05-04 10:00:00 +0900

    build: feature/main 모듈 생성

---
+++ feature/main/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    alias(libs.plugins.youhajun.android.library)
+    alias(libs.plugins.youhajun.android.library.compose)
+    alias(libs.plugins.youhajun.android.hilt)
+}
+
+android {
+    namespace = "com.youhajun.feature.main.impl"
+}
+
+dependencies {
+    implementation(projects.core.ui)
+    implementation(projects.feature.home.api)
+    implementation(projects.feature.call.api)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
