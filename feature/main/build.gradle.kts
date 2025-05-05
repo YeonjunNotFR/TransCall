b695b4786eb83d3112da14f22142109960d727c4
yeonjun <kgbduswns@gmail.com>
2025-05-05 10:00:00 +0900

    build: immutable 의존성 추가 및 main 적용

--- feature/main/build.gradle.kts
+++ feature/main/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.collections.immutable)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
