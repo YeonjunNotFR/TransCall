26dcbf18d3b528aa9360f7c9a427c1a142315c5d
yeonjun <kgbduswns@gmail.com>
2025-05-07 10:00:00 +0900

    build: core:ui 모듈 core:model, coil 의존성 추가

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -8,9 +8,11 @@ android {
 }
 
 dependencies {
+    implementation(projects.core.model)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.foundation)
+    implementation(libs.coil)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
 }
