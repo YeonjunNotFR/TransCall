94c85bff414cdfd3375a0d6fbaea39312f0706cb
yeonjun <kgbduswns@gmail.com>
2025-05-12 10:00:00 +0900

    fix: core:ui 모듈, CallHistoryItem UI 형태 수정

--- core/ui/src/main/java/com/youhajun/transcall/core/ui/components/history/CallHistoryItem.kt
+++ core/ui/src/main/java/com/youhajun/transcall/core/ui/components/history/CallHistoryItem.kt
@@ -15,9 +15,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.youhajun.core.model.CallHistory
+import com.youhajun.core.model.LanguageType
+import com.youhajun.core.model.Participant
 import com.youhajun.core.ui.R
 import com.youhajun.transcall.core.ui.components.CircleAsyncImage
 import com.youhajun.transcall.core.ui.components.HorizontalSpacer
@@ -31,81 +35,9 @@ import com.youhajun.transcall.core.ui.util.toUiDateString
 import com.youhajun.transcall.core.ui.util.toUiDurationString
 
 @Composable
-fun CallHistoryAtHomeItem(
-    callHistory: CallHistory,
-    onClickCallAgain: (String) -> Unit,
-) {
-    Row(
-        modifier = Modifier
-            .border(
-                width = 1.dp,
-                color = Colors.Gray300,
-                shape = RoundedCornerShape(12.dp)
-            )
-            .background(Color.White, shape = RoundedCornerShape(12.dp))
-            .fillMaxWidth()
-            .padding(horizontal = 12.dp, vertical = 16.dp),
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        CircleAsyncImage(
-            imageUrl = callHistory.partnerImageUrl,
-            size = 40.dp,
-        )
-
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .padding(horizontal = 8.dp)
-        ) {
-            Row {
-                Text(
-                    text = callHistory.partnerName,
-                    color = Color.Black,
-                    style = Typography.bodyLarge,
-                )
-
-                Text(
-                    text = callHistory.partnerLangType.code,
-                    color = Colors.Gray700,
-                    style = Typography.bodySmall,
-                    modifier = Modifier
-                        .padding(start = 4.dp)
-                        .align(Alignment.CenterVertically)
-                )
-            }
-
-            VerticalSpacer(4.dp)
-
-            Row {
-                Text(
-                    text = callHistory.startedAtEpochSeconds.toUiDateString(DateFormatPatterns.DATE_TIME_WITHOUT_SECONDS),
-                    color = Colors.Gray700,
-                    style = Typography.bodySmall,
-                    modifier = Modifier.alignByBaseline()
-                )
-
-                HorizontalSpacer(8.dp)
-
-                Text(
-                    text = callHistory.durationSeconds.toUiDurationString(TimeFormatPatterns.LABEL_HOUR_MIN_SEC),
-                    color = Colors.Black,
-                    style = Typography.bodySmall,
-                    modifier = Modifier.alignByBaseline()
-                )
-            }
-        }
-
-        Image(
-            painter = painterResource(id = R.drawable.ic_home),
-            contentDescription = null,
-            modifier = Modifier.size(24.dp).noRippleClickable { onClickCallAgain(callHistory.callId) }
-        )
-    }
-}
-
-@Composable
 fun CallHistoryItem(
     callHistory: CallHistory,
+    dateFormat: DateFormatPatterns = DateFormatPatterns.DATE_TIME_WITHOUT_SECONDS,
     onClickCallAgain: (String) -> Unit,
 ) {
     Row(
@@ -121,7 +53,7 @@ fun CallHistoryItem(
         verticalAlignment = Alignment.CenterVertically,
     ) {
         CircleAsyncImage(
-            imageUrl = callHistory.partnerImageUrl,
+            imageUrl = callHistory.partner?.imageUrl,
             size = 40.dp,
         )
 
@@ -130,17 +62,36 @@ fun CallHistoryItem(
                 .weight(1f)
                 .padding(horizontal = 8.dp)
         ) {
-            Text(
-                text = callHistory.partnerName,
-                color = Color.Black,
-                style = Typography.bodyLarge,
-            )
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                Text(
+                    text = callHistory.partner?.displayName ?: "",
+                    color = Color.Black,
+                    style = Typography.bodyLarge,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.weight(1f, fill = false)
+                )
+
+                HorizontalSpacer(4.dp)
+
+                Text(
+                    text = "(${callHistory.partner?.language?.code?.uppercase() ?: ""})",
+                    color = Colors.PrimaryLight,
+                    style = Typography.bodySmall.copy(
+                        fontWeight = FontWeight.W800
+                    ),
+                    softWrap = false,
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+            }
 
             VerticalSpacer(4.dp)
 
             Row {
                 Text(
-                    text = callHistory.startedAtEpochSeconds.toUiDateString(DateFormatPatterns.DATE_TIME_WITHOUT_SECONDS),
+                    text = callHistory.startedAtEpochSeconds.toUiDateString(dateFormat),
                     color = Colors.Gray700,
                     style = Typography.bodySmall,
                     modifier = Modifier.alignByBaseline()
@@ -167,30 +118,20 @@ fun CallHistoryItem(
 
 @Preview(showBackground = true)
 @Composable
-private fun CallHistoryItemAtHomePreview() {
-    CallHistoryAtHomeItem(
-        callHistory = CallHistory(
-            callId = "12345",
-            partnerName = "John Doe",
-            partnerImageUrl = "https://example.com/image.jpg",
-            startedAtEpochSeconds = 1633072800L,
-            durationSeconds = 12001,
-        ),
-        onClickCallAgain = {}
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun CallHistoryItemPreview() {
+private fun CallHistoryFullDateItemPreview() {
     CallHistoryItem(
         callHistory = CallHistory(
             callId = "12345",
-            partnerName = "John Doe",
-            partnerImageUrl = "https://example.com/image.jpg",
+            partner = Participant(
+                userId = "67890",
+                displayName = "John Doe dwaafwojfdkawdkadwkd dawkawd akwdkaw dawkd wa dwakd",
+                imageUrl = "https://example.com/image.jpg",
+                language = LanguageType.ENGLISH
+            ),
             startedAtEpochSeconds = 1633072800L,
             durationSeconds = 12001,
         ),
+        dateFormat = DateFormatPatterns.DATE_TIME_WITHOUT_SECONDS,
         onClickCallAgain = {}
     )
 }
