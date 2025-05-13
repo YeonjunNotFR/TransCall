1d724bec33ffa9b9c3cfac04fa07eed4a5c76fb7
yeonjun <kgbduswns@gmail.com>
2025-05-13 10:00:00 +0900

    feat: core:ui 모듈, LazyBackgroundColumn 생성

---
+++ core/ui/src/main/java/com/youhajun/transcall/core/ui/components/LazyBackgroundColumn.kt
@@ -0,0 +1,42 @@
+package com.youhajun.transcall.core.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.youhajun.transcall.core.ui.theme.Colors
+
+@Composable
+fun LazyBackgroundColumn(
+    modifier: Modifier = Modifier,
+    isFirst: Boolean,
+    isLast: Boolean,
+    paddingValues: PaddingValues,
+    radius: Dp = 12.dp,
+    color: Color = Colors.White,
+    content: @Composable () -> Unit,
+) {
+    val shape = when {
+        isFirst && isLast -> RoundedCornerShape(radius)
+        isFirst -> RoundedCornerShape(topStart = radius, topEnd = radius)
+        isLast -> RoundedCornerShape(bottomStart = radius, bottomEnd = radius)
+        else -> RoundedCornerShape(0.dp)
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color, shape)
+            .padding(paddingValues)
+            .then(modifier)
+    ) {
+        content()
+    }
+}
