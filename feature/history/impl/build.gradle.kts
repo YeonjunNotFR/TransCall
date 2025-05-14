14b59cb9cb0068a92e64d5295bc84ea3f81a61b7
yeonjun <kgbduswns@gmail.com>
2025-05-14 10:00:00 +0900

    feat: feature:history:impl 모듈 생성

---
+++ feature/history/impl/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    alias(libs.plugins.youhajun.android.library)
+    alias(libs.plugins.youhajun.android.library.compose)
+    alias(libs.plugins.youhajun.android.hilt)
+}
+
+android {
+    namespace = "com.youhajun.feature.history.impl"
+}
+
+dependencies {
+    implementation(projects.feature.history.api)
+    implementation(projects.domain.history)
+    implementation(projects.core.ui)
+    implementation(projects.core.model)
+    implementation(libs.bundles.navigation)
+    implementation(libs.androidx.compose.constraintlayout)
+    implementation(libs.bundles.orbit)
+    implementation(libs.kotlinx.collections.immutable)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.foundation)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
