ff506971ea22b158d970bca997bd619eb318d7f7
yeonjun <kgbduswns@gmail.com>
2025-05-15 10:00:00 +0900

    build: feature:home:impl 모듈, domain:room, domain:history, core:model 의존성 추가 및 기타 라이브러리 의존성 추가

--- feature/home/impl/build.gradle.kts
+++ feature/home/impl/build.gradle.kts
@@ -10,8 +10,16 @@ android {
 
 dependencies {
     implementation(projects.feature.home.api)
+    implementation(projects.domain.room)
+    implementation(projects.domain.history)
     implementation(projects.core.ui)
-    implementation(libs.androidx.navigation.compose)
+    implementation(projects.core.model)
+    implementation(libs.bundles.navigation)
+    implementation(libs.androidx.compose.constraintlayout)
+    implementation(libs.bundles.orbit)
+    implementation(libs.kotlinx.collections.immutable)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
