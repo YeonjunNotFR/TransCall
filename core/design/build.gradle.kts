de0f88b5596f04e5d2bacce3563db3f29c807dcb
yeonjun <kgbduswns@gmail.com>
2025-05-25 12:00:00 +0900

    feat: core:design 모듈 생성 및 디자인 파일 move

---
+++ core/design/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(libs.plugins.youhajun.android.library)
+    alias(libs.plugins.youhajun.android.library.compose)
+}
+
+android {
+    namespace = "com.youhajun.core.design"
+}
+
+dependencies {
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.compose.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+}
---
+++ core/design/src/main/java/com/youhajun/core/design/Color.kt
@@ -0,0 +1,48 @@
+package com.youhajun.core.design
+
+import androidx.compose.ui.graphics.Color
+
+object Colors {
+    // Brand Colors
+    val Primary = Color(0xFF0061A4)
+    val PrimaryLight = Color(0xFF4A90E2)
+    val PrimaryDark = Color(0xFF003B73)
+
+    val Secondary = Color(0xFF00BFA5)
+    val SecondaryLight = Color(0xFF5DF2D6)
+    val SecondaryDark = Color(0xFF008E76)
+
+    // Status Colors
+    val Success = Color(0xFF00C853)
+    val Warning = Color(0xFFFFAB00)
+    val Error = Color(0xFFD50000)
+    val Info = Color(0xFF0091EA)
+
+    // Grayscale / Neutral
+    val White = Color(0xFFFFFFFF)
+    val Black = Color(0xFF000000)
+
+    val Gray50 = Color(0xFFFAFAFA)
+    val Gray100 = Color(0xFFF5F5F5)
+    val Gray200 = Color(0xFFEEEEEE)
+    val Gray300 = Color(0xFFE0E0E0)
+    val Gray500 = Color(0xFF9E9E9E)
+    val Gray700 = Color(0xFF616161)
+    val Gray900 = Color(0xFF212121)
+
+    // Background / Surface
+    val BackgroundLight = Color(0xFFFFFFFF)
+    val BackgroundDark = Color(0xFF121212)
+    val SurfaceLight = Color(0xFFFDFDFD)
+    val SurfaceDark = Color(0xFF1E1E1E)
+
+    // Border / Outline
+    val Outline = Color(0xFFBDBDBD)
+
+    val FF50A7F3 = Color(0xFF50A7F3)
+    val FF374151 = Color(0xFF374151)
+    val FFE5E7EB = Color(0xFFE5E7EB)
+    val FF6366F1 = Color(0xFF6366F1)
+    val FF2F80ED = Color(0xFF2F80ED)
+    val FFB2F2FF = Color(0xFFD2ECFE)
+}
