d160488bec8f69434adbc6c58bb8203c3117c85e
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-06-03 12:00:00 +0900

    feat: core:design 색상 및 아이콘, 스트링 리소스 추가

--- core/design/src/main/java/com/youhajun/core/design/Color.kt
+++ core/design/src/main/java/com/youhajun/core/design/Color.kt
@@ -3,7 +3,6 @@ package com.youhajun.core.design
 import androidx.compose.ui.graphics.Color
 
 object Colors {
-    // Brand Colors
     val Primary = Color(0xFF0061A4)
     val PrimaryLight = Color(0xFF4A90E2)
     val PrimaryDark = Color(0xFF003B73)
@@ -12,16 +11,14 @@ object Colors {
     val SecondaryLight = Color(0xFF5DF2D6)
     val SecondaryDark = Color(0xFF008E76)
 
-    // Status Colors
     val Success = Color(0xFF00C853)
     val Warning = Color(0xFFFFAB00)
     val Error = Color(0xFFD50000)
     val Info = Color(0xFF0091EA)
 
-    // Grayscale / Neutral
     val White = Color(0xFFFFFFFF)
     val Black = Color(0xFF000000)
-
+    val LightGray = Color(0xFFCCCCCC)
     val Gray50 = Color(0xFFFAFAFA)
     val Gray100 = Color(0xFFF5F5F5)
     val Gray200 = Color(0xFFEEEEEE)
@@ -30,19 +27,23 @@ object Colors {
     val Gray700 = Color(0xFF616161)
     val Gray900 = Color(0xFF212121)
 
-    // Background / Surface
     val BackgroundLight = Color(0xFFFFFFFF)
     val BackgroundDark = Color(0xFF121212)
     val SurfaceLight = Color(0xFFFDFDFD)
     val SurfaceDark = Color(0xFF1E1E1E)
 
-    // Border / Outline
     val Outline = Color(0xFFBDBDBD)
 
+    val FF00FFFF = Color(0xFF00FFFF)
     val FF50A7F3 = Color(0xFF50A7F3)
     val FF374151 = Color(0xFF374151)
     val FFE5E7EB = Color(0xFFE5E7EB)
     val FF6366F1 = Color(0xFF6366F1)
     val FF2F80ED = Color(0xFF2F80ED)
     val FFB2F2FF = Color(0xFFD2ECFE)
+    val FF0A0A1A = Color(0xFF0A0A1A)
+    val FF15152B = Color(0xFF15152B)
+    val FF1F1B2C = Color(0xFF1F1B2C)
+    val FF292929 = Color(0xFF292929)
+    val FFFF0000 = Color(0xFFFF0000)
 }
