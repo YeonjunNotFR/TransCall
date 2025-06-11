202d285ce56a07f4a39c526ab6d23484c3d44bc0
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-06-11 12:00:00 +0900

    feat: data:auth TokenRefresherImpl에 MainEventManager 추가
    TokenRefresherImpl 클래스에 MainEventManager를 주입하여 토큰 갱신 실패 시
    RequireLogin 이벤트를 전송하도록 변경함. JwtTokenDto로 응답 타입도 수정.

--- data/auth/src/main/java/com/youhajun/data/auth/TokenRefresherImpl.kt
+++ data/auth/src/main/java/com/youhajun/data/auth/TokenRefresherImpl.kt
@@ -1,8 +1,10 @@
 package com.youhajun.data.auth
 
 import android.util.Log
+import com.youhajun.core.event.MainEvent
+import com.youhajun.core.event.MainEventManager
 import com.youhajun.core.network.TokenRefresher
-import com.youhajun.data.auth.dto.TokenRefreshResponse
+import com.youhajun.data.auth.dto.JwtTokenDto
 import io.ktor.client.call.body
 import io.ktor.client.plugins.auth.providers.BearerTokens
 import io.ktor.client.plugins.auth.providers.RefreshTokensParams
@@ -12,7 +14,9 @@ import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import javax.inject.Inject
 
-internal class TokenRefresherImpl @Inject constructor() : TokenRefresher {
+internal class TokenRefresherImpl @Inject constructor(
+    private val mainEventManager: MainEventManager
+) : TokenRefresher {
 
     override suspend fun refreshTokens(
         param: RefreshTokensParams,
@@ -23,11 +27,12 @@ internal class TokenRefresherImpl @Inject constructor() : TokenRefresher {
                     markAsRefreshTokenRequest()
                     contentType(ContentType.Application.Json)
                     setBody(mapOf("refresh_token" to oldTokens?.refreshToken))
-                }.body<TokenRefreshResponse>()
+                }.body<JwtTokenDto>()
 
                 BearerTokens(response.accessToken, response.refreshToken)
             } catch (e: Exception) {
                 Log.e("Ktor", "Token refresh failed", e)
+                mainEventManager.sendEvent(MainEvent.RequireLogin)
             }
             return null
         }
