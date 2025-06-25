b5bd9c9945cbb4d68f1f62fbf32edb66b522f8df
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-06-25 12:00:00 +0900

    refactor: feature:home:api, HomeNavGraphRegister 제거 및 HomeNavRoute 에 DeepLinkRoute 적용

--- feature/home/api/build.gradle.kts
+++ feature/home/api/build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
     alias(libs.plugins.youhajun.android.library)
-    alias(libs.plugins.youhajun.android.library.compose)
-    alias(libs.plugins.youhajun.android.hilt)
     alias(libs.plugins.kotlin.serialization)
 }
 
@@ -10,7 +8,6 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.navigation.compose)
     implementation(libs.kotlinx.serialization.json)
-    implementation(projects.core.model)
+    implementation(projects.core.route)
 }
--- feature/home/api/src/main/java/com/youhajun/feature/home/api/HomeNavRoute.kt
+++ feature/home/api/src/main/java/com/youhajun/feature/home/api/HomeNavRoute.kt
@@ -1,9 +1,18 @@
 package com.youhajun.feature.home.api
 
-import com.youhajun.core.model.navigation.TransCallRoute
+import android.net.Uri
+import com.youhajun.core.route.DeepLinkRoute
+import com.youhajun.core.route.PathTemplate
+import com.youhajun.core.route.TransCallRoute
 import kotlinx.serialization.Serializable
 
 sealed interface HomeNavRoute : TransCallRoute {
     @Serializable
-    data object Home : HomeNavRoute
+    data object Home : HomeNavRoute, DeepLinkRoute {
+        override val pathTemplate = PathTemplate("home")
+
+        override fun uriToRoute(uri: Uri): TransCallRoute? {
+            return if (pathTemplate.match(uri) != null) this else null
+        }
+    }
 }
