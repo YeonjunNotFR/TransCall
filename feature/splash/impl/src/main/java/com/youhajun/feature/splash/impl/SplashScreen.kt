b85e7dad0e29c1e18f21c1cc1f3b3d0ef0d57b3f
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-07-03 12:00:00 +0900

    feat: feature:splash SplashScreen.kt 에 SplashPreviewMirror 추가

--- feature/splash/impl/src/main/java/com/youhajun/feature/splash/impl/SplashScreen.kt
+++ feature/splash/impl/src/main/java/com/youhajun/feature/splash/impl/SplashScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.youhajun.core.design.Colors
 import com.youhajun.core.design.R
@@ -67,4 +68,10 @@ internal fun SplashRoute() {
             )
         }
     }
+}
+
+@Composable
+@Preview
+private fun SplashPreviewMirror() {
+    SplashPreview()
 }
