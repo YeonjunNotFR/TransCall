77e0820a7550ad6724f246191e92cfbf393322e2
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-07-07 12:00:00 +0900

    feat: feature:auth:impl GoogleAuthManager 생성 및 GoogleIdTokenCredential 로직 구현
    GoogleAuthManagerImpl 클래스를 생성하여 Google 로그인 및 로그아웃 기능을 구현했습니다.
    GoogleIdTokenCredential을 사용하여 Google ID 토큰을 가져오고 처리합니다.
    GoogleAuthModule을 통해 ActivityComponent에 GoogleAuthManager를 바인딩합니다.
    LocalGoogleAuthManager를 제공하여 Composable에서 GoogleAuthManager에 접근할 수 있도록 합니다.

---
+++ feature/auth/impl/src/main/java/com/youhajun/feature/auth/impl/util/GoogleAuthManagerImpl.kt
@@ -0,0 +1,59 @@
+package com.youhajun.feature.auth.impl.util
+
+import android.content.Context
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.credentials.ClearCredentialStateRequest
+import androidx.credentials.CredentialManager
+import androidx.credentials.CustomCredential
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.GetCredentialResponse
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.youhajun.feature.auth.api.GoogleAuthManager
+import com.youhajun.feature.auth.impl.BuildConfig
+import dagger.hilt.android.qualifiers.ActivityContext
+import dagger.hilt.android.scopes.ActivityScoped
+import javax.inject.Inject
+
+@ActivityScoped
+class GoogleAuthManagerImpl @Inject constructor(
+    @ActivityContext private val context: Context,
+) : GoogleAuthManager {
+
+    private val credentialManager = CredentialManager.create(context)
+
+    override suspend fun signIn(nonce: String): Result<String> = runCatching {
+        val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
+            .setFilterByAuthorizedAccounts(true)
+            .setServerClientId(BuildConfig.GOOGLE_WEB_CLIENT_ID)
+            .setAutoSelectEnabled(true)
+            .setNonce(nonce)
+            .build()
+
+        val request: GetCredentialRequest = GetCredentialRequest.Builder()
+            .addCredentialOption(googleIdOption)
+            .build()
+
+        val result = credentialManager.getCredential(request = request, context = context)
+        val idToken = handleSignIn(result)
+        idToken ?: throw Exception("Failed to find Google ID Token")
+    }
+
+    override suspend fun signOut(): Result<Unit> = runCatching {
+        val clearRequest = ClearCredentialStateRequest()
+        credentialManager.clearCredentialState(clearRequest)
+    }
+
+    private fun handleSignIn(result: GetCredentialResponse): String? {
+        val credential = result.credential
+        return if (credential is CustomCredential && credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
+            credential.type
+            val googleIdTokenCredential = GoogleIdTokenCredential.createFrom(credential.data)
+            googleIdTokenCredential.idToken
+        } else null
+    }
+}
+
+val LocalGoogleAuthManager = staticCompositionLocalOf<GoogleAuthManager> {
+    error("No GoogleAuthManager provided")
+}
