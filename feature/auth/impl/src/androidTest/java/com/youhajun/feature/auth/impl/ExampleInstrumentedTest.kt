2a9085493cc344bbe771160d566d37c2b727b1fc
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-07-07 12:00:00 +0900

    feat: feature:auth:impl 모듈 생성 및 의존성 추가
    - `feature:auth:impl` 모듈을 생성하였습니다.
    - `AndroidManifest.xml`, `build.gradle.kts`, `.gitignore`, `proguard-rules.pro`, `consumer-rules.pro` 파일을 추가하였습니다.
    - `GOOGLE_WEB_CLIENT_ID`를 BuildConfig에 추가하였습니다.
    - 필요한 의존성들을 추가하였습니다. (home.api, auth.api, domain.auth, core.ui, core.design, core.model, core.route, credentials, navigation, orbit, kotlinx.collections.immutable, compose.material3, compose.foundation, test, androidTest)
    - 기본 유닛 테스트 및 Instrumented 테스트 파일을 추가하였습니다.

--- feature/auth/impl/src/androidTest/java/com/youhajun/feature/auth/impl/ExampleInstrumentedTest.kt
+++ feature/auth/impl/src/androidTest/java/com/youhajun/feature/auth/impl/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.youhajun.feature.room.impl
+package com.youhajun.feature.auth.impl
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
