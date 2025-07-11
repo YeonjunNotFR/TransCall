1f377b85b70e24012f3fec730fef6139ca3cb581
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-07-11 12:00:00 +0900

    feat: core:route NavigationEvent 타입 추가 및 NavigationEventHandler 기존 구조 개선
    NavigationEvent에 새로운 네비게이션 이벤트 추가 및 기존 이벤트 구조를 개선함.
    NavigationEventHandler에서 네비게이션 로직을 정리하고, popUpRoute 처리 방식을 수정함.

--- core/route/src/main/java/com/youhajun/core/route/NavigationEvent.kt
+++ core/route/src/main/java/com/youhajun/core/route/NavigationEvent.kt
@@ -4,16 +4,27 @@ sealed interface NavigationEvent {
 
     data class Navigate(
         val route: TransCallRoute,
-        val saveState: Boolean,
         val launchSingleTop: Boolean,
+    ) : NavigationEvent
+
+    data class NavigateAndSave(
+        val route: TransCallRoute,
+        val launchSingleTop: Boolean,
+        val popUpRoute: TransCallRoute
+    ) : NavigationEvent
+
+    data class NavigateBottomBar(
+        val route: TransCallRoute,
+        val launchSingleTop: Boolean,
+    ) : NavigationEvent
+
+    data class NavigateAndClear(
+        val route: TransCallRoute,
+        val launchSingleTop: Boolean,
+        val popUpRoute: TransCallRoute? = null
     ) : NavigationEvent
 
     data class NavigateWeb(val url: String) : NavigationEvent
 
     data object NavigateBack : NavigationEvent
-
-    data class NavigateAndClear(
-        val route: TransCallRoute,
-        val launchSingleTop: Boolean,
-    ) : NavigationEvent
 }
--- core/route/src/main/java/com/youhajun/core/route/NavigationEventHandler.kt
+++ core/route/src/main/java/com/youhajun/core/route/NavigationEventHandler.kt
@@ -2,6 +2,8 @@ package com.youhajun.core.route
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -11,35 +13,57 @@ class NavigationEventHandler(
 ) {
     fun handleNavigationEvent(event: NavigationEvent) {
         when (event) {
-            NavigationEvent.NavigateBack -> {
-                navController.popBackStack()
-            }
 
             is NavigationEvent.Navigate -> {
                 navController.navigate(event.route) {
-                    if (event.saveState) {
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            saveState = true
-                        }
-                        restoreState = true
+                    launchSingleTop = event.launchSingleTop
+                }
+            }
+
+            is NavigationEvent.NavigateAndSave -> {
+                navController.navigate(event.route) {
+                    popUpTo(event.popUpRoute) {
+                        saveState = true
                     }
+                    restoreState = true
                     launchSingleTop = event.launchSingleTop
                 }
             }
 
             is NavigationEvent.NavigateAndClear -> {
                 navController.navigate(event.route) {
-                    popUpTo(navController.graph.findStartDestination().id) {
+                    val popUpRoute = event.popUpRoute ?: navController.currentNestedGraphStartId()
+                    popUpTo(0) {
                         inclusive = true
                     }
                     launchSingleTop = event.launchSingleTop
                 }
             }
 
+            is NavigationEvent.NavigateBottomBar -> {
+                navController.navigate(event.route) {
+                    popUpTo(navController.currentNestedGraphStartId()) {
+                        saveState = true
+                    }
+                    restoreState = true
+                    launchSingleTop = event.launchSingleTop
+                }
+            }
+
+            NavigationEvent.NavigateBack -> {
+                navController.popBackStack()
+            }
+
             is NavigationEvent.NavigateWeb -> {
 
             }
         }
+    }
+
+    private fun NavController.currentNestedGraphStartId(): Int {
+        val currentDestination = currentBackStackEntry?.destination
+        val parentGraph = currentDestination?.parent ?: graph
+        return parentGraph.findStartDestination().id
     }
 }
 
