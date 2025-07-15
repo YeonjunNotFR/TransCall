78c3c6bb81e15908c79c0da22595153ee3c7ffea
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-07-15 12:00:00 +0900

    feat: feature:main MainBottomBar 컴포넌트 추가 및 리팩토링
    MainBottomBar 컴포넌트를 새로 생성하고, 기존 MainScreen에서 하단 바를
    구현하여 탭 네비게이션을 지원함. MainTab의 구조를 변경하여 중앙 탭
    구현을 추가하고, UI 요소를 개선함.

---
+++ feature/main/src/main/java/com/youhajun/feature/main/navigation/MainBottomBar.kt
@@ -0,0 +1,187 @@
+package com.youhajun.feature.main.navigation
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import com.youhajun.core.design.Colors
+import com.youhajun.core.design.Typography
+import com.youhajun.transcall.core.ui.components.VerticalSpacer
+import com.youhajun.transcall.core.ui.components.modifier.noRippleClickable
+import com.youhajun.transcall.core.ui.components.modifier.topBorder
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
+
+@Composable
+internal fun MainBottomBar(
+    visibility: Boolean,
+    currentTab: MainTab,
+    tabs: ImmutableList<MainTab>,
+    onClickMainTab: (MainTab) -> Unit,
+) {
+    AnimatedVisibility(
+        visible = visibility,
+        enter = fadeIn() + slideIn { IntOffset(0, it.height) },
+        exit = fadeOut() + slideOut { IntOffset(0, it.height) }
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(54.dp)
+                .topBorder(1.dp, Colors.FFE5E7EB),
+        ) {
+            tabs.forEach { tab ->
+                if (tab.isCenter) {
+                    CenterTabItem(
+                        tab = tabs.first { it.isCenter },
+                        isSelected = currentTab.isCenter,
+                        onClick = onClickMainTab,
+                    )
+                } else {
+                    MainTabItem(
+                        tab = tab,
+                        isSelected = tab.route == currentTab.route,
+                        onClick = onClickMainTab,
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun RowScope.MainTabItem(
+    tab: MainTab,
+    isSelected: Boolean,
+    onClick: (MainTab) -> Unit,
+) {
+    val textColor = if (isSelected) Colors.FF637BE8 else Colors.FF808080
+    val iconTintColor = if (isSelected) Colors.FF98AAFE else Colors.FF808080
+
+    Column(
+        Modifier
+            .weight(1f)
+            .fillMaxHeight()
+            .noRippleClickable { onClick(tab) },
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Icon(
+            modifier = Modifier.size(24.dp),
+            painter = painterResource(id = tab.iconResId),
+            tint = iconTintColor,
+            contentDescription = null
+        )
+
+        VerticalSpacer(4.dp)
+
+        Text(
+            text = stringResource(tab.stringResId),
+            color = textColor,
+            style = Typography.bodySmall
+        )
+
+        VerticalSpacer(5.dp)
+    }
+}
+
+@Composable
+private fun RowScope.CenterTabItem(
+    tab: MainTab,
+    isSelected: Boolean,
+    onClick: (MainTab) -> Unit,
+) {
+    Box(
+        modifier = Modifier
+            .weight(1f)
+            .fillMaxHeight()
+            .noRippleClickable { onClick(tab) },
+        contentAlignment = Alignment.Center,
+    ) {
+
+        val textColor = if (isSelected) Colors.FF637BE8 else Colors.FF808080
+
+        Box(
+            modifier = Modifier
+                .size(48.dp)
+                .offset(y = (-24).dp)
+                .shadow(
+                    elevation = 4.dp,
+                    shape = CircleShape,
+                    ambientColor = Colors.FF2F80ED,
+                    spotColor = Colors.FF2F80ED,
+                    clip = false
+                )
+                .background(Colors.FF98AAFE, CircleShape)
+        ) {
+            Icon(
+                modifier = Modifier
+                    .size(28.dp)
+                    .align(Alignment.Center),
+                painter = painterResource(id = tab.iconResId),
+                tint = Colors.White,
+                contentDescription = null
+            )
+        }
+        
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            VerticalSpacer(4.dp)
+
+            Text(
+                text = stringResource(tab.stringResId),
+                color = textColor,
+                style = Typography.bodySmall
+            )
+
+            VerticalSpacer(5.dp)
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun MainBottomBarPreview() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Transparent),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        MainBottomBar(
+            visibility = true,
+            currentTab = MainTab.Home,
+            tabs = MainTab.tabList().toImmutableList(),
+            onClickMainTab = {}
+        )
+    }
+}
--- feature/main/src/main/java/com/youhajun/feature/main/navigation/MainTab.kt
+++ feature/main/src/main/java/com/youhajun/feature/main/navigation/MainTab.kt
@@ -1,21 +1,25 @@
 package com.youhajun.feature.main.navigation
 
 import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hasRoute
 import com.youhajun.core.route.TransCallRoute
 import com.youhajun.core.design.R
+import com.youhajun.feature.auth.api.AuthNavRoute
 import com.youhajun.feature.history.api.HistoryNavRoute
 import com.youhajun.feature.home.api.HomeNavRoute
 
 enum class MainTab(
     val route: TransCallRoute,
     val order: Int,
-    @DrawableRes val iconResId: Int
+    val isCenter: Boolean,
+    @DrawableRes val iconResId: Int,
+    @StringRes val stringResId: Int,
 ) {
-    Home(HomeNavRoute.Home, 0, R.drawable.ic_home),
-    History(HistoryNavRoute.HistoryList, 1, R.drawable.ic_home),
-    MyPage(HomeNavRoute.Home, 2, R.drawable.ic_home);
+    History(HistoryNavRoute.HistoryList, 0, false, R.drawable.ic_home, R.string.bottom_bar_history),
+    Home(HomeNavRoute.Home, 1, true, R.drawable.ic_home, R.string.bottom_bar_home),
+    MyPage(AuthNavRoute.Login, 2, false, R.drawable.ic_home, R.string.bottom_bar_profile);
 
     companion object {
         fun tabList(): List<MainTab> = entries.sortedBy { it.order }
