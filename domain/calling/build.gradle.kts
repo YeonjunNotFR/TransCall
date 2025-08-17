a0eefe4d891921df06524a06cf6894a112c8505f
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-08-17 12:00:00 +0900

    refactor: domain:calling user 모듈 의존성 제거
    domain:calling 모듈에서 사용하지 않는 domain:user 모듈 의존성을 제거했습니다.

--- domain/calling/build.gradle.kts
+++ domain/calling/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
 
 dependencies {
     implementation(projects.core.model)
-    implementation(projects.domain.user)
     implementation(libs.javax.inject)
     implementation(libs.kotlinx.coroutines.core)
 
