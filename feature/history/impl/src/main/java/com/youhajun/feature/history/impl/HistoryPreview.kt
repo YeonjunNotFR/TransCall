e1c4fec5440da452bacc2aa49b8b4935e05d929d
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-08-22 12:00:00 +0900

    refactor: feature:history HistoryPreview 모델 변경
    HistoryPreview에서 사용되던 Participant 모델을 CurrentParticipant 모델로
    변경함.

--- feature/history/impl/src/main/java/com/youhajun/feature/history/impl/HistoryPreview.kt
+++ feature/history/impl/src/main/java/com/youhajun/feature/history/impl/HistoryPreview.kt
@@ -5,7 +5,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.youhajun.core.model.LanguageType
 import com.youhajun.core.model.calling.CallHistory
-import com.youhajun.core.model.room.Participant
+import com.youhajun.core.model.room.CurrentParticipant
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.persistentMapOf
 
@@ -20,8 +20,8 @@ internal fun HistoryPreview() {
                         historyId = "12345",
                         title = "Sample Call History Title",
                         summary = "This is a sample call history summary that is quite long and should be truncated if it exceeds the maximum line limit.",
-                        participants = persistentListOf(
-                            Participant(
+                        currentParticipants = persistentListOf(
+                            CurrentParticipant(
                                 userId = "67890",
                                 displayName = "John Doe dwaafwojfdkawdkadwkd dawkawd akwdkaw dawkd wa dwakd",
                                 imageUrl = "https://example.com/image.jpg",
@@ -38,8 +38,8 @@ internal fun HistoryPreview() {
                         historyId = "67890",
                         title = "Another Call History",
                         summary = "This is another call history summary that is also quite long and should be truncated if it exceeds the maximum line limit.",
-                        participants = persistentListOf(
-                            Participant(
+                        currentParticipants = persistentListOf(
+                            CurrentParticipant(
                                 userId = "12345",
                                 displayName = "Jane Smith",
                                 imageUrl = "https://example.com/image2.jpg",
@@ -58,8 +58,8 @@ internal fun HistoryPreview() {
                         historyId = "11223",
                         title = "Third Call History",
                         summary = "This is the third call history summary, which is also quite long and should be truncated if it exceeds the maximum line limit.",
-                        participants = persistentListOf(
-                            Participant(
+                        currentParticipants = persistentListOf(
+                            CurrentParticipant(
                                 userId = "33445",
                                 displayName = "Alice Johnson",
                                 imageUrl = "https://example.com/image3.jpg",
