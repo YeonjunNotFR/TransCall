fee2dc081969326e6f9b31380472e7eb360f9b82
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-09-14 12:00:00 +0900

    feat: core:database BuildConfig 및 KSP 플러그인 추가
    `core/database` 모듈에 BuildConfig 사용을 활성화하고, KSP(Kotlin Symbol Processing) 플러그인을 추가했습니다. 또한, Timber 라이브러리를 의존성에 추가했습니다.
    
    주요 변경 사항:
    - `build.gradle.kts`에 `com.google.devtools.ksp` 플러그인 적용
    - `buildFeatures { buildConfig = true }`를 설정하여 BuildConfig 활성화
    - Timber 의존성 추가

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -1,19 +1,22 @@
 plugins {
     alias(libs.plugins.youhajun.android.library)
     alias(libs.plugins.youhajun.android.hilt)
+    alias(libs.plugins.ksp)
 }
 
 android {
     namespace = "com.youhajun.core.database"
+
+    buildFeatures {
+        buildConfig = true
+    }
 }
 
 dependencies {
-
     implementation(libs.bundles.dataStore)
     implementation(libs.bundles.room)
     ksp(libs.room.compiler)
-    implementation(libs.room.ktx)
-    implementation(libs.room.runtime)
+    implementation(libs.timber)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
