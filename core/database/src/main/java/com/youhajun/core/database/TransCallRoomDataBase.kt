a141a0632e12d850a6cc46e0d3b3fdd7eac125dc
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-09-15 12:00:00 +0900

    refactor: core:database Room 데이터베이스 구조 변경
    `RoomDataBase` 파일명을 `TransCallRoomDataBase`로 변경하여 프로젝트의 목적을 명확히 하고, 데이터베이스 스키마를 확장했습니다.
    
    주요 변경 사항:
    - 기존 `RoomDataBase.kt` 파일 삭제
    - `TransCallRoomDataBase.kt` 파일 추가
    - 새로운 Entity(`ConversationMetaEntity`, `ConversationCursorEntity`, `ParticipantEntity`) 추가
    - 관련 Dao(`ConversationMetaDao`, `ConversationCursorDao`, `ParticipantDao`) 추가
    - 데이터베이스 버전을 4로 업데이트

---
+++ core/database/src/main/java/com/youhajun/core/database/TransCallRoomDataBase.kt
@@ -0,0 +1,25 @@
+package com.youhajun.core.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.youhajun.core.database.dao.ConversationCursorDao
+import com.youhajun.core.database.dao.ConversationDao
+import com.youhajun.core.database.dao.ConversationMetaDao
+import com.youhajun.core.database.dao.ParticipantDao
+import com.youhajun.core.database.entity.ConversationCursorEntity
+import com.youhajun.core.database.entity.ConversationEntity
+import com.youhajun.core.database.entity.ConversationMetaEntity
+import com.youhajun.core.database.entity.ParticipantEntity
+
+@Database(
+    entities = [ConversationEntity::class, ConversationMetaEntity::class, ConversationCursorEntity::class, ParticipantEntity::class],
+    version = 4
+)
+@TypeConverters
+internal abstract class TransCallRoomDataBase : RoomDatabase() {
+    abstract fun conversationMetaDao(): ConversationMetaDao
+    abstract fun conversationCursorDao(): ConversationCursorDao
+    abstract fun conversationDao(): ConversationDao
+    abstract fun participantDao(): ParticipantDao
+}
