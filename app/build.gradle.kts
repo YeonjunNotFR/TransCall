85b6b342f6d940d3b112202d45fc1489574cdcb4
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-09-17 12:00:00 +0900

    feat: app 모듈 의존성 및 플러그인 설정
    최상위 `app` 모듈에 필요한 플러그인과 프로젝트 모듈 의존성을 추가했습니다.
    
    **주요 변경 사항:**
    - **플러그인 추가:**
      - `youhajun.android.firebase`: Firebase 관련 설정을 위한 플러그인
      - `ksp`: 코틀린 심볼 프로세싱(Kotlin Symbol Processing) 플러그인
      - `kotlin.serialization`: 코틀린 직렬화 플러그인
    - **의존성 추가:**
      - `feature`, `domain`, `data` 계층의 모든 모듈 의존성 추가
      - `core` 계층의 모든 모듈(`route`, `network`, `common`, `webRtc`, `database`, `notification`) 의존성 추가
      - 외부 라이브러리 추가: `kotlinx-serialization-json`, `firebase-messaging`, `timber`, `workmanager`, `hilt-compiler` (ksp)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
     alias(libs.plugins.youhajun.android.application)
     alias(libs.plugins.youhajun.android.application.compose)
+    alias(libs.plugins.youhajun.android.firebase)
     alias(libs.plugins.youhajun.android.hilt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -9,6 +12,35 @@ android {
 }
 
 dependencies {
+    implementation(projects.feature.main)
+    implementation(projects.feature.call)
+    implementation(projects.feature.history)
+    implementation(projects.feature.history.api)
+    implementation(projects.feature.home)
+    implementation(projects.domain.history)
+    implementation(projects.domain.room)
+    implementation(projects.domain.auth)
+    implementation(projects.domain.calling)
+    implementation(projects.domain.user)
+    implementation(projects.domain.conversation)
+    implementation(projects.data.history)
+    implementation(projects.data.room)
+    implementation(projects.data.auth)
+    implementation(projects.data.calling)
+    implementation(projects.data.user)
+    implementation(projects.data.conversation)
+    implementation(projects.core.route)
+    implementation(projects.core.network)
+    implementation(projects.core.common)
+    implementation(projects.core.webRtc)
+    implementation(projects.core.database)
+    implementation(projects.core.notification)
+
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.firebase.messaging)
+    implementation(libs.timber)
+    implementation(libs.bundles.workmanager)
+    ksp(libs.androidx.hilt.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
