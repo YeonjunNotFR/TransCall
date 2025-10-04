a1ade55ffa661729fc02b9522e493f96e2780d41
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-10-04 12:00:00 +0900

    chore: domain:calling 모듈 의존성 추가
    `domain:conversation`, `domain:room`, `kotlinx-collections-immutable` 의존성을 추가하여 관련 도메인 모듈 및 불변 컬렉션을 사용할 수 있도록 설정했습니다.

--- domain/calling/build.gradle.kts
+++ domain/calling/build.gradle.kts
@@ -3,8 +3,11 @@ plugins {
 }
 
 dependencies {
+    implementation(projects.domain.conversation)
+    implementation(projects.domain.room)
     implementation(projects.core.model)
     implementation(libs.javax.inject)
     implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.collections.immutable)
 
 }
