bb20906317e9dceaec8bb6907ff881613bd1b80c
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-10-18 12:00:00 +0900

    refactor: feature:call 모듈 구조 변경
    `feature:call:impl` 모듈의 `navigation` 패키지를 `feature:call` 모듈로 이동하여 모듈 구조를 개선했습니다.
    
    주요 변경 사항:
    - `CallNavGraph.kt` 파일을 `feature:call:impl`에서 `feature:call` 모듈로 이동
    - 관련 import 경로 수정

--- feature/call/src/main/java/com/youhajun/feature/call/navigation/CallNavGraph.kt
+++ feature/call/src/main/java/com/youhajun/feature/call/navigation/CallNavGraph.kt
@@ -1,10 +1,10 @@
-package com.youhajun.feature.call.impl.navigation
+package com.youhajun.feature.call.navigation
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.youhajun.core.route.NavigationEvent
 import com.youhajun.feature.call.api.CallNavRoute
-import com.youhajun.feature.call.impl.calling.CallingRoute
+import com.youhajun.feature.call.calling.CallingRoute
 
 fun NavGraphBuilder.callNavGraph(
     onNavigate: (NavigationEvent) -> Unit,
