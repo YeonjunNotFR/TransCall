44bdc6b3cff8d3735c3be05e0798be845ff7491c
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-11-06 12:16:55 +0900

    feat: core:model Compose 안정성 어노테이션 추가
    `TranslationResponse` 인터페이스와 `TranslationMessage` 데이터 클래스에 `@Stable` 어노테이션을 추가하여 Compose 런타임의 안정성을 보장하고 불필요한 리컴포지션을 방지했습니다.

--- core/model/src/main/java/com/youhajun/core/model/calling/payload/TranslationResponse.kt
+++ core/model/src/main/java/com/youhajun/core/model/calling/payload/TranslationResponse.kt
@@ -1,12 +1,14 @@
 package com.youhajun.core.model.calling.payload
 
+import androidx.compose.runtime.Stable
 import com.youhajun.core.model.LanguageType
 import com.youhajun.core.model.conversation.ConversationState
 
+@Stable
 sealed interface TranslationResponse : ResponsePayload
-
 data object SttStart : TranslationResponse
 
+@Stable
 data class TranslationMessage(
     val conversationId: String,
     val roomId: String,
